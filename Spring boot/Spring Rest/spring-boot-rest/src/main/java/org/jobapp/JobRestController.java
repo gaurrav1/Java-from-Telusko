e78206a6b3c2ecb5762832d48e36f7e9d6b783d1
Gaurav <22amtics009@gmail.com>
2024-08-19 12:12:46 +0530

    Spring boot rest

---
+++ Spring boot/Spring Rest/spring-boot-rest/src/main/java/org/jobapp/JobRestController.java
@@ -0,0 +1,43 @@
+package org.jobapp;
+
+import org.jobapp.model.JobPost;
+import org.jobapp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000/")
+public class JobRestController {
+
+    @Autowired
+    private JobService jobService;
+
+    @GetMapping("/jobs")
+    public List<JobPost> getAllJobs() {
+        List<JobPost> jobs = jobService.getAllJobs();
+        return jobs;
+    }
+
+    @GetMapping(path = "/job/{postId}", produces = {"application/xml"})
+    public JobPost getJob(@PathVariable("postId") int postId) {
+        return jobService.getJob(postId);
+    }
+
+    @PostMapping(path = "/job", consumes = {"application/xml"})
+    public void addJob(@RequestBody JobPost jobPost) {
+        jobService.addJob(jobPost);
+    }
+
+    @PutMapping("/job")
+    public void updateJob(@RequestBody JobPost jobPost) {
+        jobService.updateJob(jobPost);
+    }
+
+    @DeleteMapping("/job/{postId}")
+    public void deleteJob(@PathVariable int postId){
+        jobService.deleteJob(postId);
+    }
+
+}
---
+++ Spring boot/Spring Rest/spring-boot-rest/src/main/java/org/jobapp/service/JobService.java
@@ -0,0 +1,39 @@
+package org.jobapp.service;
+
+import org.jobapp.model.JobPost;
+import org.jobapp.repository.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo jobRepo;
+
+    public void addJob(JobPost jobPost){
+
+        jobRepo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs() {
+
+        List<JobPost> jobPost = jobRepo.getAllJobs();
+        return jobPost;
+    }
+
+    public JobPost getJob(int postId) {
+        return jobRepo.getJob(postId);
+    }
+
+    public void updateJob(JobPost jobPost) {
+        jobRepo.updateJob(jobPost);
+    }
+
+    public void deleteJob(int postId) {
+        jobRepo.deleteJob(postId);
+    }
+}
+
